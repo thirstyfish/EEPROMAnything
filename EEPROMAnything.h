b3492d5220609b82d06fbc3eba622e98ff2a9c82
thirsty fish <feky001@gmail.com>
2014-09-19 09:15:18 +1000

    Changed back to using underlying EEPROM.h

--- EEPROMAnything.h
+++ EEPROMAnything.h
@@ -4,17 +4,25 @@
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
 	    const byte* p = (const byte*)(const void*)&value;
-	        unsigned int i;
-		    for (i = 0; i < sizeof(value); i++)
-			              EEPROM.write(ee++, *p++);
-		        return i;
+	    unsigned int i;
+
+			for (i = 0; i < sizeof(value); i++){
+			  if( EEPROM.read(ee)==*p ){
+			    ee++;
+			    p++;
+			  }else{
+			    EEPROM.write(ee++, *p++);  // Only write the data if it is different to what's there
+			  }
+			}
+
+			return i;
 }
 
 template <class T> int EEPROM_readAnything(int ee, T& value)
 {
 	    byte* p = (byte*)(void*)&value;
-	        unsigned int i;
-		    for (i = 0; i < sizeof(value); i++)
-			              *p++ = EEPROM.read(ee++);
-		        return i;
+      unsigned int i;
+	    for (i = 0; i < sizeof(value); i++)
+        *p++ = EEPROM.read(ee++);
+      return i;
 }
